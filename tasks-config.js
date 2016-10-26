29c8fba4c7ce4d668fc3b490dfeb99063478ba6f
action jackson <action@localhost.localdomain>
2016-10-26 16:26:37 -0600

    git unignore tasks-config.js npm ignore tasks-config.js

---
+++ tasks-config.js
@@ -0,0 +1,11 @@
+let pckg = require('./package.json');
+
+let configs = {
+  'package': pckg,
+  'tsc': require('./tsconfig.json'),
+  'TMP_DIR': 'tmp',
+  'IN_DIR': 'src',
+  'OUT_DIR': pckg.name
+}
+
+module.exports = configs;
