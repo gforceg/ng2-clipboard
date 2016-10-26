2c61546a92a79da2fa056d1cc6833ccc37b5d8b2
action jackson <action@localhost.localdomain>
2016-10-26 12:47:55 -0600

    updated .gitignore

--- tmp/clipboard.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Clipboard } from 'ts-clipboard/ts-clipboard';
-
-@Injectable()
-export class ClipboardService {
-	constructor() {}
-	// clipboard = {copy: function(str:string) { console.log('copy %s', str); } };
-	copy(str: string): void { Clipboard.copy(str); };
-}
--- tmp/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './clipboard.service';
-export * from './component/clipboard.component';
-export * from './clipboard.module';
