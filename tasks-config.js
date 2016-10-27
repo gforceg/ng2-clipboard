80e19ed8e82818ce46e0989c88c35216033636a5
action jackson <action@localhost.localdomain>
2016-10-27 16:50:24 -0600

    fixed spelling error in tsconfig-aot.json locked in @angular/*@2.1.0 changed TMP_DIR to '.tmp' rather than 'tmp'

--- tasks-config.js
+++ tasks-config.js
@@ -3,7 +3,7 @@ let pckg = require('./package.json');
 let configs = {
   'package': pckg,
   'tsc': require('./tsconfig.json'),
-  'TMP_DIR': 'tmp',
+  'TMP_DIR': '.tmp',
   'IN_DIR': 'src',
   'OUT_DIR': `${pckg.name}`,
   'BUNDLE_DIR': 'bundles',
