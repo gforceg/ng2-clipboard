434409cf37dcc64d93b069023684eded7701340d
action jackson <action@localhost.localdomain>
2016-10-27 16:33:06 -0600

    updated 'todo' task

--- gulp-tasks/todo.js
+++ gulp-tasks/todo.js
@@ -6,7 +6,7 @@ let config = require('../tasks-config.js');
 var todo = require('gulp-todo');
 
 gulp.task('todo', () => {
-  gulp.src(['**/*.{ts,html,scss}', 'gulpfile.js', '!node_modules/**/*', `!${config.package.name}/**/*`, `!${config.TMP_DIR}/**/*`])
+  gulp.src(['**/*.{ts,html,scss}', 'gulp-tasks/*.js', 'gulpfile.js', '!node_modules/**/*', `!${config.package.name}/**/*`, `!${config.TMP_DIR}/**/*`])
   .pipe(todo())
   .pipe(gulp.dest('./'))
 });
