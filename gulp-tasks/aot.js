f4ad3aeb21dfb9d500b9eba81feb0230816b4804
action jackson <action@localhost.localdomain>
2016-11-03 09:47:25 -0600

    aot task is platform agnostic..

--- gulp-tasks/aot.js
+++ gulp-tasks/aot.js
@@ -1,8 +1,8 @@
 let gulp = require('gulp');
 let gutil = require('gulp-util');
-
+let path = require('path');
 let execSync = require('child_process').execSync;
 
 gulp.task('aot', () => {
-  return execSync('node_modules/.bin/ngc -p tsconfig-aot.json');
+  return execSync(`${path.join('node_modules', '.bin', 'ngc')} -p tsconfig-aot.json`);
 });
