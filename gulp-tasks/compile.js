a2b2da97d4fa228f5233cac44e0a929a3e422acf
action jackson <action@localhost.localdomain>
2016-11-29 10:20:45 -0700

    compile root barrel on 'compile' task.

--- gulp-tasks/compile.js
+++ gulp-tasks/compile.js
@@ -7,14 +7,14 @@ let inline = require('gulp-inline-ng2-template');
 let ts = require('gulp-typescript');
 let sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sourcemaps', ['tsc'], () => {
+gulp.task('sourcemaps', ['tsc out folder'], () => {
   return gulp.src(join(config.OUT_DIR, '**/*.js'))
   .pipe(sourcemaps.init())
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest(config.OUT_DIR));
 })
 
-gulp.task('tsc', () => {
+gulp.task('tsc out folder', () => {
 
   return gulp.src(join(config.OUT_DIR, '**/*.ts'))
   .pipe(inline({ useRelativePaths: true }))
@@ -22,4 +22,12 @@ gulp.task('tsc', () => {
   .pipe(gulp.dest(config.OUT_DIR));
 });
 
-gulp.task('compile', ['tsc', 'sourcemaps'], () => { } );
+gulp.task('tsc root barrel', () => {
+
+  return gulp.src(join(process.cwd(), `${config.package_config.name}.ts`))
+  // .pipe(inline({ useRelativePaths: true }))
+  .pipe(ts(config.tsc_config.compilerOptions))
+  .pipe(gulp.dest(process.cwd()));
+});
+
+gulp.task('compile', ['tsc out folder', 'tsc root barrel', 'sourcemaps'], () => { } );
