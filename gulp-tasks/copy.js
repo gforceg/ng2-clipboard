f44426fb6bb6718f32ba0b592fbb7c8793368866
action jackson <action@localhost.localdomain>
2016-11-28 21:13:23 -0700

    inline scss and css files.

--- gulp-tasks/copy.js
+++ gulp-tasks/copy.js
@@ -6,6 +6,6 @@ let join = require('path').join;
 let config = require('../config/tasks-config.js');
 
 gulp.task('copy', () => {
-  return gulp.src(join(config.IN_DIR, '**/*.{ts,html,scss}'))
+  return gulp.src(join(config.IN_DIR, '**/*.{ts,html,scss,css}'))
   .pipe(gulp.dest(config.TMP_DIR));
 });
