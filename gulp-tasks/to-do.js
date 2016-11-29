cd5cfd63d1c813d37eecc48dd81afdd4bd9b3dca
action jackson <action@localhost.localdomain>
2016-11-28 17:18:48 -0700

    rename gulp task 'todo' to 'to-do'

--- gulp-tasks/to-do.js
+++ gulp-tasks/to-do.js
@@ -4,7 +4,7 @@ let config = require('../config/tasks-config.js');
 
 var todo = require('gulp-todo');
 
-gulp.task('todo', () => {
+gulp.task('to-do', () => {
   gulp.src(['**/*.{ts,html,scss}', 'gulp-tasks/*.js', 'gulpfile.js', '!node_modules/**/*', `!${config.package_config.name}/**/*`, `!${config.TMP_DIR}/**/*`])
   .pipe(todo())
   .pipe(gulp.dest('./'))
