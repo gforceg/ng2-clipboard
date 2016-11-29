0645e04f2eb27f7cc8a2c1f00f0fc61d984ce6f8
action jackson <action@localhost.localdomain>
2016-11-29 11:39:59 -0700

    ditto

--- config/tasks-config.js
+++ config/tasks-config.js
@@ -1,17 +1,18 @@
-let pckg = require('../package.json');
-let bundle_config = require('./bundle-config.json');
+let pckg = require("../package.json");
+let bundle_config = require("./bundle-config.json");
 
 let configs = {
-  'package_config': pckg,
-  'tsc_config': require('./tsconfig.json'),
-  'aot_config': require('./tsconfig-aot.json'),
-  'bundle_config': bundle_config,
-  'TMP_DIR': '.tmp',
-  'IN_DIR': 'src',
-  'OUT_DIR': `${pckg.name}`,
-  'BUNDLE_DIR': 'bundles',
-  'FACTORY_DIR': 'ngfactory',
-  'ts_and_js_files_excluded_from_cleaning': [
+  "package_config": pckg,
+  "tsc_config": require("../tsconfig.json"),
+  "aot_config": require("../tsconfig-aot.json"),
+  "bundle_config": bundle_config,
+  "TMP_DIR": ".tmp",
+  "IN_DIR": "src",
+  "OUT_DIR": `${pckg.name}`,
+  "CONFIG_DIR": "config",
+  "BUNDLE_DIR": "bundles",
+  "FACTORY_DIR": "ngfactory",
+  "ts_and_js_files_excluded_from_cleaning": [
     "!gulpfile.js",
     "!bundlefile.js"
   ]
